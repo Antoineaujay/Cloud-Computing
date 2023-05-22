529145a659412b9dddd12fca51916b64acb8df9a
Antoineaujay <antoine.aujay@gmail.com>
2023-05-22 18:08:19 +0100

    maj

---
+++ action.php
@@ -0,0 +1,39 @@
+<?php
+// Récupérer les valeurs du formulaire
+$prenom = $_POST['prenom'];
+$nom = $_POST['nom'];
+$pseudo = $_POST['pseudo'];
+$ville = $_POST['ville'];
+$pays = $_POST['pays'];
+$cp = $_POST['cp'];
+
+// Connexion à la base de données
+$serverName = 'cloudcomputingserverantoinerayan.database.windows.net';
+$connectionOptions = array(
+    'Database' => 'CloudComputing',
+    'Uid' => 'antoineaujay',
+    'PWD' => 'Cloudcomputing.',
+);
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+
+if ($conn === false) {
+    die(print_r(sqlsrv_errors(), true));
+}
+
+// Requête d'insertion des données
+$sql = "INSERT INTO donnees (prenom, nom, pseudo, ville, pays, cp) VALUES (?, ?, ?, ?)";
+$params = array($prenom, $nom, $pseudo, $ville, $pays, $cp);
+$stmt = sqlsrv_query($conn, $sql, $params);
+
+if ($stmt === false) {
+    die(print_r(sqlsrv_errors(), true));
+}
+
+echo "Données insérées avec succès.";
+
+// Fermer la connexion
+sqlsrv_close($conn);
+// Redirect to index.php
+header("Location: formulaire.php");
+exit();
+?>
--- formulaire.php
+++ formulaire.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,38 +8,38 @@
 </head>
      <body>
          <div class="container">
-             <h1>Formulaires</h1>
-             <form class="needs-validation" novalidate>
+             <h1>Formulaire</h1>
+             <form action="action.php" class="needs-validation" novalidate>
                  <div class="form-row">
                      <div class="col-md-4 mb-3">
                          <label for="prenom">Prénom</label>
-                         <input type="text" class="form-control" id="prenom" placeholder="Pierre" required>
+                         <input type="text" class="form-control" id="prenom" name="prenom" placeholder="Pierre" required>
                          
                      </div>
                      <div class="col-md-4 mb-3">
                          <label for="nom">Nom de famille</label>
-                         <input type="text" class="form-control" id="nom" placeholder="Giraud" required>
+                         <input type="text" class="form-control" id="nom" name="nom placeholder="Giraud" required>
                          
                      </div>
                      <div class="col-md-4 mb-3">
                          <label for="pseudo">Pseudo</label>
-                         <input type="text" class="form-control" id="pseudo" placeholder="PierreGird" required>
+                         <input type="text" class="form-control" id="pseudo" name="pseudo" placeholder="PierreGird" required>
                          
                      </div>
                  </div>
                  <div class="form-row">
                      <div class="col-md-6 mb-3">
                          <label for="ville">Ville</label>
-                         <input type="text" class="form-control" id="ville" placeholder="Ville" required>
+                         <input type="text" class="form-control" id="ville" name="ville" placeholder="Ville" required>
                      </div>
                      <div class="col-md-3 mb-3">
                          <label for="pays">Pays</label>
-                         <input type="text" class="form-control" id="pays" placeholder="Pays" required>
+                         <input type="text" class="form-control" id="pays" name="pays" placeholder="Pays" required>
                          
                      </div>
                      <div class="col-md-3 mb-3">
                          <label for="cp">Code postal</label>
-                         <input type="number" class="form-control" id="cp" placeholder="Code postal" required>
+                         <input type="number" class="form-control" id="cp" name="cp" placeholder="Code postal" required>
                          
                      </div>
                  </div>
